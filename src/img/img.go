604a22cf2323d7203fed01b37e1c86b68074cd8a
drguigui1 <pierre.guillaume@epita.fr>
2021-03-20 23:59:19 +0100

    Img struct type

---
+++ src/img/img.go
@@ -0,0 +1,19 @@
+package img
+
+import (
+    "volumetrical-cloud/vector3"
+)
+
+type Img struct {
+    NbRows int
+    NbCols int
+    Pixels []vector3.Vector3
+}
+
+func (img *Img) SetPixel(i, j int, color vector3.Vector3) {
+    img.Pixels[i * img.NbCols + j] = color
+}
+
+func (img Img) GetPixel(i, j int) vector3.Vector3 {
+    return img.Pixels[i * img.NbCols + j]
+}
--- src/main.go
+++ src/main.go
@@ -5,10 +5,14 @@ import (
 
     "volumetrical-cloud/vector3"
     "volumetrical-cloud/ray"
+    "volumetrical-cloud/img"
 )
 
 func main() {
     ray := ray.InitRay(vector3.InitVector3(1, 1, 1), vector3.InitVector3(2, 2, 2))
     fmt.Println(ray)
     fmt.Println(vector3.InitVector3(1.0 / 14.0, 2.0 / 14.0, 3.0 / 14.0).Length())
+
+    img := img.Img{0, 0, []vector3.Vector3{}}
+    fmt.Println(img)
 }
