b4480e67f926fd2ba27c3d4fafa1eeaff7c18d10
drguigui1 <pierre.guillaume@epita.fr>
2021-03-22 09:37:59 +0100

    Voxel grid in progress

---
+++ src/voxel_grid/voxel_grid.go
@@ -0,0 +1,42 @@
+package voxel_grid
+
+import (
+    "volumetric-cloud/vector3"
+)
+
+/*
+** One voxel == one cube
+** 'Density': density in the voxel
+** 'Transmitivity': how the voxel transmit the light
+** 'Color': color inside the voxel
+*/
+type Voxel struct {
+    Density float64
+    Transmitivity float64
+    Color vector3.Vector3
+}
+
+/*
+** 3D grid of voxels
+** 'VoxelSize': size of one voxel (one cube)
+*/
+type VoxelGrid struct {
+    VoxelSize float64
+    nbVoxelX int
+    nbVoxelY int
+    nbVoxelZ int
+
+    Voxels []Voxel
+}
+
+func InitVoxel(density, transmitivity float64, color vector3.Vector3) Voxel {
+    return Voxel{
+        Density: density,
+        Transmitivity: transmitivity,
+        Color: color,
+    }
+}
+
+func InitVoxelGrid() VoxelGrid {
+    // TODO
+}
