fcd1e55fa1a5d1614480c3aa1caf94dbc5dc682b
Louis GUO <louis.guo@op3ft.org>
2021-03-24 01:55:41 +0100

    [Go Routine] added a little bit of optimisation for go routine

--- src/scene/scene.go
+++ src/scene/scene.go
@@ -27,34 +27,33 @@ func (s Scene) Render(imgSizeY, imgSizeX int) img.Img {
 
     // create the wait group
     wg := sync.WaitGroup{}
-    wg.Add(imgSizeY * imgSizeX)
+    wg.Add(imgSizeY)
 
     for i := 0; i < imgSizeY; i += 1 {
-        for j := 0; j < imgSizeX; j += 1 {
-            go s.renderPixel(image, i, j, &wg)
-            //image.SetPixel(i, j, 200, 100, 20)
-        }
+        go s.renderPixel(image, i, imgSizeX, &wg)
+        //image.SetPixel(i, j, 200, 100, 20)
     }
+    wg.Wait()
 
     return image
 }
 
-func (s Scene) renderPixel(image img.Img, i, j int, wg *sync.WaitGroup) {
+func (s Scene) renderPixel(image img.Img, i, imgSizeX int, wg *sync.WaitGroup) {
+    for j := 0; j < imgSizeX; j += 1 {
+        // create the ray
+        ray := s.Camera.CreateRay(i, j)
 
-    // create the ray
-    ray := s.Camera.CreateRay(i, j)
+        // Check intersect with Voxel Grid
+        _, hasHit, color := s.VoxelGrid.IntersectFaces(ray)
 
-    // Check intersect with Voxel Grid
-    _, hasHit, color := s.VoxelGrid.IntersectFaces(ray)
+        // raymarch TODO
 
-    // raymarch TODO
-
-    // set pixel
-    if hasHit {
-        image.SetPixel(i, j, byte(color.X), byte(color.Y), byte(color.Z))
-    } else {
-        image.SetPixel(i, j, 255, 255, 255)
+        // set pixel
+        if hasHit {
+            image.SetPixel(i, j, byte(color.X), byte(color.Y), byte(color.Z))
+        } else {
+            image.SetPixel(i, j, 255, 255, 255)
+        }
     }
-
     wg.Done()
 }
