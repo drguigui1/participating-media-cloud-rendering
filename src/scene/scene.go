fc084c9de8950a4a3f1a1262c97b06b25427c194
Louis GUO <louis.guo@op3ft.org>
2021-04-03 14:51:02 +0200

    added 2x faster go routine on loop on imagesizeY

--- src/scene/scene.go
+++ src/scene/scene.go
@@ -43,17 +43,85 @@ func (s Scene) Render(imgSizeY, imgSizeX, nbRaysPerPixel int) img.Img {
 
     // create the wait group
     wg := sync.WaitGroup{}
-    wg.Add(imgSizeY * imgSizeX)
+    wg.Add(imgSizeY)
 
     for i := 0; i < imgSizeY; i += 1 {
+        go s.renderImageSizeY(image, i, imgSizeX, nbRaysPerPixel, &wg)
+
+        /*
         for j := 0; j < imgSizeX; j += 1 {
             //go s.renderPixel(image, i, j, nbRaysPerPixel, &wg)
             s.renderPixel(image, i, j, nbRaysPerPixel, nil)
         }
+        */
+
     }
+    wg.Wait()
     return image
 }
 
+
+
+func (s Scene) renderImageSizeY(image img.Img, i, imgSizeX, nbRaysPerPixel int, wg *sync.WaitGroup) {
+    for j := 0; j < imgSizeX; j += 1 {
+        color := vector3.InitVector3(0, 0, 0)
+        for k := 0; k < nbRaysPerPixel; k += 1 {
+            // create the ray
+            r := s.Camera.CreateRay(float64(j) + rand.Float64(), float64(i) + rand.Float64())
+
+            var accColor vector3.Vector3
+            var accC vector3.Vector3
+
+            var accTransparency float64 = 1.0
+            var accT float64
+
+            var hasHit bool
+            var hasOneHit bool = false
+
+            // Check intersect with Voxel Grids
+            for _, vGrid := range s.VoxelGrids {
+                accC, accT, hasHit = vGrid.ComputePixelColor(r, s.Light.Color)
+                if !hasHit {
+                    continue
+                } else {
+                    hasOneHit = true
+                }
+
+                // accumulate transparency
+                accTransparency *= accT
+
+                // accumulate color
+                accColor.AddVector3(accC)
+            }
+
+            // get background impact
+            backgroundColor := background.RenderGradient(r)
+
+            // set pixel
+            if hasOneHit {
+                // compute pizel color
+                backgroundColorImpact := vector3.MulVector3Scalar(backgroundColor, accTransparency)
+                accColor.AddVector3(backgroundColorImpact)
+                accColor.Clamp(0.0, 1.0)
+                color.AddVector3(vector3.InitVector3(accColor.X, accColor.Y, accColor.Z))
+            } else {
+                // gradient case
+                color.AddVector3(backgroundColor)
+            }
+        }
+
+        // divide color vector by nbRaysPerPixel
+        color.Div(float64(nbRaysPerPixel))
+
+        // Set the pixel color
+        image.SetPixel(i, j, byte(color.X * 255.0), byte(color.Y * 255.0), byte(color.Z * 255.0))
+
+
+    }
+    wg.Done()
+}
+
+/*
 func (s Scene) renderPixel(image img.Img, i, j, nbRaysPerPixel int, wg *sync.WaitGroup) {
     color := vector3.InitVector3(0, 0, 0)
     for k := 0; k < nbRaysPerPixel; k += 1 {
@@ -111,3 +179,5 @@ func (s Scene) renderPixel(image img.Img, i, j, nbRaysPerPixel int, wg *sync.Wai
         wg.Done()
     }
 }
+
+ */
