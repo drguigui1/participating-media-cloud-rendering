dcc400b29ef66ab4257b8cbb69630a02f9757c93
drguigui1 <pierre.guillaume@epita.fr>
2021-04-07 15:12:39 +0200

    Refacto code -> random cloud generation

--- src/main.go
+++ src/main.go
@@ -3,9 +3,6 @@ package main
 import (
     "math"
     "fmt"
-    "math/rand"
-    "sort"
-    "time"
 
     "volumetric-cloud/camera"
     "volumetric-cloud/light"
@@ -13,6 +10,7 @@ import (
     "volumetric-cloud/scene"
     "volumetric-cloud/vector3"
     "volumetric-cloud/voxel_grid"
+    //"volumetric-cloud/random_clouds"
 )
 
 func main() {
@@ -22,17 +20,6 @@ func main() {
     // Camera
     aspectRatio := float64(imgSizeX) / float64(imgSizeY)
     fieldOfView := math.Pi / 2
-/*    origin := vector3.InitVector3(-12, 6, -13)
-    camera := camera.InitCamera(
-       aspectRatio,
-       fieldOfView,
-       imgSizeX,
-       imgSizeY,
-       origin,
-       -math.Pi / 8,
-       -math.Pi / 2,
-       0.0,
-    )*/
 
     origin := vector3.InitVector3(0, 15, 5)
     camera := camera.InitCamera(
@@ -46,35 +33,14 @@ func main() {
        0.0,
     )
 
-    minX := -100
-    maxX := 100
-    minY := 30
-    maxY := 40
-    minZ := -300
-    maxZ := -10
-    numberClouds := 5
-    voxelGrids := make([]voxel_grid.VoxelGrid, numberClouds)
+    //voxelGrids := random_clouds.GenerateRandomClouds(3)
+    shift := vector3.InitVector3(-31.0, 31.0, -148.0)
+    oppositeCorner := vector3.InitVector3(17.0, 38.0, -64.0)
+    var seed int64 = 42
+    perlinNoise := noise.InitPerlinNoise(0.2, 2.0, 1.0, 0.5, 3, seed)
+    voxelGrid := voxel_grid.InitVoxelGrid(0.4, shift, oppositeCorner, 0.15, perlinNoise)
 
-    for i := 0; i < numberClouds; i++ {
-        rand.Seed(time.Now().UnixNano())
-        shiftX := []float64 { float64(rand.Intn(maxX - minX) + minX), float64(rand.Intn(maxX - minX) + minX) }
-        shiftZ := []float64 { float64(rand.Intn(maxZ - minZ) + minZ), float64(rand.Intn(maxZ - minZ) + minZ) }
-        shiftY := []float64 { float64(rand.Intn(maxY - minY) + minY), float64(rand.Intn(maxY - minY) + minY) }
-        sort.Float64s(shiftX)
-        sort.Float64s(shiftZ)
-        sort.Float64s(shiftY)
-        perlinNoise := noise.InitPerlinNoise(0.2, 2.0, 1.0, 0.5, 3, int64(i))
-        shift := vector3.InitVector3(shiftX[0], shiftY[0], shiftZ[0])
-        oppositeCorner := vector3.InitVector3(shiftX[1], shiftY[1], shiftZ[1])
-
-        voxelGrids[i] = voxel_grid.InitVoxelGrid(0.2, shift, oppositeCorner, 0.12, perlinNoise)
-        fmt.Println("shift")
-        fmt.Println(shift)
-
-        fmt.Println("opposite")
-        fmt.Println(oppositeCorner)
-    }
-
+    voxelGrids := []voxel_grid.VoxelGrid{voxelGrid}
 /*
     // Voxel Grid 1
     shift := vector3.InitVector3(-20.0, 35.0, -90.0)
@@ -119,8 +85,8 @@ func main() {
     fmt.Println("VOXEL")
 
     // Lights
-    light1 := light.InitLight(vector3.InitVector3(0.0, 200.0, 0.0), vector3.InitVector3(0.25, 0.25, 0.25))
-    light2 := light.InitLight(vector3.InitVector3(0.0, 0.0, 0.0), vector3.InitVector3(0.2, 0.2, 0.2))
+    light1 := light.InitLight(vector3.InitVector3(0.0, 200.0, -200.0), vector3.InitVector3(0.27, 0.27, 0.27))
+    light2 := light.InitLight(vector3.InitVector3(0.0, 0.0, 0.0), vector3.InitVector3(0.15, 0.15, 0.15))
     //light2 := light.InitLight(vector3.InitVector3(-50.0, 150.0, -100.0), vector3.InitVector3(0.4, 0.4, 0.4))
     //light3 := light.InitLight(vector3.InitVector3(0.0, 0.0, 0.0), vector3.InitVector3(0.3, 0.3, 0.3))
     //light4 := light.InitLight(vector3.InitVector3(100.0, 100.0, 100.0), vector3.InitVector3(0.4, 0.4, 0.4))
---
+++ src/random_clouds/generate.go
@@ -0,0 +1,47 @@
+package random_clouds
+
+import (
+    "math/rand"
+    "sort"
+    "fmt"
+    "time"
+
+    "volumetric-cloud/voxel_grid"
+    "volumetric-cloud/noise"
+    "volumetric-cloud/vector3"
+)
+
+func GenerateRandomClouds(nbClouds int) []voxel_grid.VoxelGrid {
+    minX := -100
+    maxX := 100
+    minY := 30
+    maxY := 40
+    minZ := -300
+    maxZ := -10
+    voxelGrids := make([]voxel_grid.VoxelGrid, nbClouds)
+
+    for i := 0; i < nbClouds; i++ {
+        rand.Seed(time.Now().UnixNano())
+
+        shiftX := []float64 { float64(rand.Intn(maxX - minX) + minX), float64(rand.Intn(maxX - minX) + minX) }
+        shiftZ := []float64 { float64(rand.Intn(maxZ - minZ) + minZ), float64(rand.Intn(maxZ - minZ) + minZ) }
+        shiftY := []float64 { float64(rand.Intn(maxY - minY) + minY), float64(rand.Intn(maxY - minY) + minY) }
+
+        sort.Float64s(shiftX)
+        sort.Float64s(shiftZ)
+        sort.Float64s(shiftY)
+
+        perlinNoise := noise.InitPerlinNoise(0.2, 2.0, 1.0, 0.5, 3, int64(i))
+        shift := vector3.InitVector3(shiftX[0], shiftY[0], shiftZ[0])
+        oppositeCorner := vector3.InitVector3(shiftX[1], shiftY[1], shiftZ[1])
+
+        voxelGrids[i] = voxel_grid.InitVoxelGrid(0.2, shift, oppositeCorner, 0.15, perlinNoise)
+        fmt.Println("shift")
+        fmt.Println(shift)
+
+        fmt.Println("opposite")
+        fmt.Println(oppositeCorner)
+    }
+
+    return voxelGrids
+}
